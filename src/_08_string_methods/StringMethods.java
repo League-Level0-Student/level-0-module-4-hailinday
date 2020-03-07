bf9414ba7c9ed8aa99b3ae1dc45b2b97799551a6
League Student <league@iMac-20.attlocal.net>
2020-03-06 16:57:57 -0800

    Pet

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -8,23 +8,29 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
-		String hi;
+		String abc = "abc";
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-		
+		System.out.println(abc.charAt(2));
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+		System.out.println(abc.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+		for (int i = 0; i < 3; i++) {
+			System.out.println(abc.charAt(i));
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+		for (int i = 0; i < 3; i++) {
+			if (abc.charAt(i) == 'c') {
+				System.out.println(abc.charAt(i) + " is at index " + i);
+			}
+		}
 	}
 }
 
--- src/_10_happy_pet/HappyPet.java
+++ src/_10_happy_pet/HappyPet.java
@@ -5,26 +5,50 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	static int happinessLevel = 0;
 	public static void main(String[] args) {
 		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
+		String pet = JOptionPane.showInputDialog("What pet do you want?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
-
+		for (int i = 0; i < ; i++) {
+			
+		
+		int task = JOptionPane.showOptionDialog(null, "How will you make your pet happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "give it a plane", "give it a train", "give it a car" }, null);
+			
 			// 5. Use user input to call the appropriate method created in step 4.
-
+			if (task==0) {
+				plane();
+			} else if (task==1) {
+				train();
+			} else if (task==2) {
+				car();
+			}
 			// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
-
+			if (happinessLevel>=5) {
+				JOptionPane.showMessageDialog(null,"Your pet loves you... I guess.");
+				break;
+			}
 	}
-
+	}
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+	public static void plane() {
+		JOptionPane.showMessageDialog(null,"It seems to be obsessed with the plane.");
+		happinessLevel = happinessLevel + 3;
+	}
+	public static void train() {
+		JOptionPane.showMessageDialog(null,"It like the train.");
+		happinessLevel = happinessLevel + 2;
+	}
+	public static void car() {
+		JOptionPane.showMessageDialog(null,"It kinda likes the car.");
+		happinessLevel = happinessLevel + 1;
+	}
 }
